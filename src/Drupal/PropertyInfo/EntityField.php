d5a5841977ef6a4678970aa093d06cb96bf78481
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-06-20 09:49:47 +0200

    skeleton (no code) for Drupal entity property info extractor

---
+++ src/Drupal/PropertyInfo/EntityField.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MakinaCorpus\Dashboard\Drupal\PropertyInfo;
+
+/**
+ * This is basically a hack that will allow property info to access Drupal's
+ * entity weird array structure via the ProperyInfo component, thus allowing
+ * field introspection as builtin types, so we can render them.
+ */
+class EntityField
+{
+}
